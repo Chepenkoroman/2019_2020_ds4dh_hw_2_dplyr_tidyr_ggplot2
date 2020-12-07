c6934424ba1b18edbcba8540e3c41b04cb671590
Chepenko Roman <35574101+Chepenkoroman@users.noreply.github.com>
2020-12-07 16:12:17 +0300

    Update task_2.1.R
    дз

--- task_2.1.R
+++ task_2.1.R
@@ -2,10 +2,19 @@
 
 library("tidyverse")
 
-dog_names <- ...("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw_2_dplyr_tidyr_ggplot2/master/data/dog_names.csv")
+dog_names <- read.csv("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw_2_dplyr_tidyr_ggplot2/master/data/dog_names.csv")
 
-dog_names %>% 
-  ...
-  ggplot(...) +
-  ... +
-  scale_fill_brewer(palette="Dark2")
+dog_names %>%
+  group_by(sex) %>%
+  filter(sex != "Unknown") %>%
+  count(name) %>%
+  top_n(20) %>%
+  mutate(name = factor(name, levels = name)) %>%
+  ggplot(aes(fct_reorder(name, n),n, name, fill = sex)) +
+  geom_col() +
+  facet_wrap(~sex, scales = "free_y") +
+  coord_flip() +
+  labs(x = "", y = "count",
+       caption = "data from https://pudding.cool/2019/10/shelters") +
+  scale_fill_brewer(palette = "Dark2")
+#На всякий случай, была добавлена ссылка на источник (https://pudding.cool/2019/10/shelters)
