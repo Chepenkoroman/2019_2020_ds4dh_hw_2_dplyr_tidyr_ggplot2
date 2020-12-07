1838dfb8832adeee27d3c6578ff42d54abf4fa44
Chepenko Roman <35574101+Chepenkoroman@users.noreply.github.com>
2020-12-07 16:09:35 +0300

    Update task_2.2.R

--- task_2.2.R
+++ task_2.2.R
@@ -2,7 +2,16 @@
 
 library("tidyverse")
 
-dog_names <- ...("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw_2_dplyr_tidyr_ggplot2/master/data/dog_names.csv")
+dog_names <- read.csv("https://raw.githubusercontent.com/r-classes/2019_2020_ds4dh_hw_2_dplyr_tidyr_ggplot2/master/data/dog_names.csv")
 
-dog_names %>% 
-  ...
+dog_names %>%
+  tibble() %>%
+  group_by(sex, age) %>%
+  filter(sex != "Unknown") %>%
+  count(age) %>%
+  pivot_wider(names_from = sex, values_from = n) %>%
+  relocate(Male, .after = age) -> t
+
+t %>%
+  mutate(Male = Male/sum(t$Male),
+         Female = Female/sum(t$Female))
